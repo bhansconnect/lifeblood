304aafc5703032dc914b209d30664507e68c7007
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2020-08-02 21:47:14 -0700

    Add generic 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>Not Found</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </div>
+  )
+}
+
+export default NotFoundPage
